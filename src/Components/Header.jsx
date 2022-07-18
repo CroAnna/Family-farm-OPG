5de5b91b0ce701a904714158e915f449d08d3254
Ana Skarica <ana.skarica.as@gmail.com>
2022-07-18 11:40:32 +0200

    fisrt commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>OPG Skarica</h1>
+      <h3>Samo najbolje neki tu tekst</h3>
+    </div>
+  );
+};
+
+export default Header;
