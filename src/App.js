db82f9419ca287ea900d9c32b399163c0ba85d83
Ana Skarica <ana.skarica.as@gmail.com>
2022-07-18 14:54:56 +0200

    added mobile navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./App.scss";
 import Header from "./Components/Header";
 
 function App() {
+  const [openNav, setOpenNav] = useState(false);
   return (
     <div className="App">
-      <Header />
+      <Header openNav={openNav} setOpenNav={setOpenNav} />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faBars } from "@fortawesome/free-solid-svg-icons";
+import slika from "../Images/kokice.jpg";
 
-const Header = () => {
+const Header = ({ openNav, setOpenNav }) => {
+  const navHandler = () => {
+    setOpenNav(!openNav);
+  };
+
   return (
-    <div>
-      <h1>OPG Skarica</h1>
-      <h3>Samo najbolje neki tu tekst</h3>
+    <div className="header">
+      <div className="nav-bar">
+        <div>
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={navHandler}
+            className={openNav ? "hidden" : "iconBar fixed"}
+          />
+          <FontAwesomeIcon
+            icon={faX}
+            onClick={navHandler}
+            className={openNav ? "iconBar abs" : "hidden"}
+          />
+        </div>
+        <div>
+          <nav className={openNav ? "nav-bar" : "hidden"}>
+            <ul>
+              <li>Početna</li>
+              <li>O nama</li>
+              <li>Kontakt</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="cover">
+        <h1>OPG Škarica</h1>
+        <h3>Samo najbolje neki tu tekst</h3>
+        {/* <img src={slika} alt="" /> */}
+      </div>
     </div>
   );
 };
